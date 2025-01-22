f3040bf11d9649fedef1edfef60b04be45b28663
1mumble1 <mrt47@yandex.ru>
2025-01-22 08:37:08 +0300

    lw6

--- Lexer/Lexer/Lexer.py
+++ Lexer/Lexer/Lexer.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 class Token:
     def __init__(self, type_, lexeme, line, column):
@@ -12,20 +13,20 @@ class Token:
 
 class PascalLexer:
     def __init__(self, filename):
-        self.file = open(filename, 'r', encoding="utf-8")
+        self.file = open(filename, 'r')
         self.line = 0
         self.column = 0
         self.current_line = ""
         self.keywords = {"array", "begin", "else", "end", "if", "of", "or",
                          "program", "procedure", "then", "type", "var"}
-        self.operators = {"*", "+", "-", "/", ";", ",", "(", ")", "[", "]", "=",
-                          ">", "<", "<=", ">=", "<>", ":", ":=", "."}
+        self.operators = {"*", "+", "-", "/", ";", ",", "(", ")", "[", "]", "=", ">", "<", "<=", ">=", "<>", ":", ":=", "."}
+        self.separators = {"\"", "(", ")", "+", "-", "\t", "\n", " ", ";", ",", ".", "[", "]", "{", "}", "*", "/", "'", ":"}
 
     def next_token(self):
         while True:
-            if not self.current_line:  # Чтение новой строки
+            if not self.current_line:
                 self.current_line = self.file.readline()
-                if not self.current_line:  # EOF
+                if not self.current_line:
                     return None
                 self.line += 1
                 self.column = 0
@@ -33,81 +34,142 @@ class PascalLexer:
             while self.column < len(self.current_line):
                 char = self.current_line[self.column]
 
-                if char.isspace():  # Пропускаем пробелы
+                # пропуск пробельных символов
+                if char.isspace():
                     self.column += 1
                     continue
 
-                # Обработка однострочных комментариев (//)
+                # пропуск линейных комментов
                 if char == "/" and self.peek(1) == "/":
-                    start_col = self.column
-                    lexeme = self.read_until("\n").rstrip("\n")
-                    return Token("LINE_COMMENT", lexeme, self.line, start_col + 1)
+                    self.read_until("\n")
+                    continue
 
-                # Обработка блочных комментариев ({ ... })
-                # Блочные комментарии
+                # пропуск блочных комментов до }, иначе отправляем токен BAD
                 if char == "{":
+                    is_valid_block_comment = False
+                    block_comment = char
                     start_line = self.line
                     start_col = self.column
-                    lexeme = char
                     self.column += 1
-
-                    while True:
-                        # Если достигли конца текущей строки, читаем следующую строку
+                    while not is_valid_block_comment:
                         if self.column >= len(self.current_line):
                             self.current_line = self.file.readline()
                             if not self.current_line:
-                                # Если файл закончился, а закрывающая скобка так и не найдена
-                                return Token("BAD", lexeme, start_line, start_col + 1)
+                                return Token("BAD", block_comment, start_line, start_col)
                             self.line += 1
                             self.column = 0
                             continue
 
                         char = self.current_line[self.column]
-                        lexeme += char
+                        block_comment += char
                         self.column += 1
 
-                        # Завершение блочного комментария
                         if char == "}":
-                            return Token("BLOCK_COMMENT", lexeme, start_line, start_col + 1)
+                            is_valid_block_comment = True
+                
+                # строковые литералы
+                if char == "'":
+                    start_col = self.column
+                    lexeme = self.read_string()
+                    if lexeme[len(lexeme) - 1] != "'":
+                        return Token("BAD", lexeme.strip("\n"), self.line, start_col + 1)
+                    return Token("STRING", lexeme, self.line, start_col + 1)
 
-                # Ключевые слова и идентификаторы
+                # целые числа либо числа с плавающей точкой
+                if char.isdigit():
+                    start_col = self.column
+                    lexeme = self.read_while(lambda c: c not in self.separators)
+                    if not lexeme.isdigit():
+                        return Token("BAD", lexeme, self.line, start_col + 1)
+                    if self.peek(0) == '.':
+                        self.column += 1
+                        float_part = self.read_while(lambda c: c not in self.separators)
+                        if not float_part:
+                            return Token("BAD", lexeme + '.', self.line, start_col + 1)
+                        if self.peek(0) in {'+', '-'}:
+                            sign = self.peek(0)
+                            self.column += 1
+                            range_of_exp = self.read_while(lambda c: c not in self.separators)
+                            if not range_of_exp:
+                                return Token("BAD", lexeme + '.' + float_part + sign, self.line, start_col + 1)
+                            float_part += sign + range_of_exp
+                        match = re.fullmatch(r"\d+(\.\d+)?([eE][+-]?\d+)?", lexeme + '.' + float_part)
+                        if match:
+                            return Token("FLOAT", lexeme + '.' + float_part, self.line, start_col + 1)
+                        else:
+                            return Token("BAD", lexeme + '.' + float_part, self.line, start_col + 1)
+
+                    if len(lexeme) > 16:
+                        return Token("BAD", lexeme, self.line, start_col + 1)
+                    return Token("INTEGER", lexeme, self.line, start_col + 1)
+
+                # идентификатор либо ключевое слово
                 if char.isalpha() or char == "_":
                     start_col = self.column
-                    lexeme = self.read_while(lambda c: c.isalnum() or c == "_")
+                    lexeme = self.read_while(lambda c: c not in self.separators)
+                    match = re.fullmatch(r"[A-Za-z_][A-Za-z_0-9]*", lexeme)
+                    if not match:
+                        return Token("BAD", lexeme, self.line, start_col + 1)
                     token_type = lexeme.upper() if lexeme.lower() in self.keywords else "IDENTIFIER"
                     return Token(token_type, lexeme, self.line, start_col + 1)
 
-                # Числа (целые и вещественные)
-                if char.isdigit():
-                    start_col = self.column
-                    lexeme = self.read_while(lambda c: c.isdigit() or c == ".")
-                    if ".." in lexeme:
-                        self.column -= len(lexeme) - lexeme.index("..")
-                        lexeme = lexeme[:lexeme.index("..")]
-                    if lexeme.count(".") > 1:
-                        return Token("BAD", lexeme, self.line, start_col + 1)
-                    token_type = "FLOAT" if "." in lexeme else "INTEGER"
-                    return Token(token_type, lexeme, self.line, start_col + 1)
-
-                # Операторы и знаки пунктуации
+                # оператор
                 if char in self.operators:
                     start_col = self.column
                     lexeme = self.read_operator()
                     token_type = self.get_operator_type(lexeme)
                     return Token(token_type, lexeme, self.line, start_col + 1)
 
-                # Строки (однострочные в одинарных кавычках)
-                if char == "'":
-                    start_col = self.column
-                    lexeme = self.read_string()
-                    return Token("STRING", lexeme, self.line, start_col + 1)
-
-                # Некорректные токены
+                # ошибочная лексема
                 start_col = self.column
-                self.column += 1
-                return Token("BAD", char, self.line, start_col + 1)
+                bad_lexeme = self.read_while(lambda c: c not in self.separators)
+                return Token("BAD", bad_lexeme, self.line, start_col + 1)
 
-            self.current_line = ""  # Переход к следующей строке
+            self.current_line = ""
+                  
+
+        #         # Ключевые слова и идентификаторы
+        #         if char.isalpha() or char == "_":
+        #             start_col = self.column
+        #             lexeme = self.read_while(lambda c: c.isalnum() or c == "_")
+        #             token_type = lexeme.upper() if lexeme.lower() in self.keywords else "IDENTIFIER"
+        #             return Token(token_type, lexeme, self.line, start_col + 1)
+
+        #         # Числа (целые и вещественные)
+        #         if char.isdigit():
+        #             start_col = self.column
+        #             lexeme = self.read_while(lambda c: c.isdigit() or c == ".")
+        #             if self.peek(0) == 'e' and self.peek(1) in {'+', '-'}:
+        #                 lexeme += 'e' + self.peek(1)
+        #                 self.column += 2
+        #                 float_range = self.read_while(lambda c: c.isdigit())
+        #                 if not float_range:
+        #                     return Token("BAD", lexeme, self.line, start_col + 1)
+        #                 return Token("FLOAT", lexeme + float_range, self.line, start_col + 1)
+
+        #             if ".." in lexeme:
+        #                 self.column -= len(lexeme) - lexeme.index("..")
+        #                 lexeme = lexeme[:lexeme.index("..")]
+        #             if lexeme.count(".") > 1:
+        #                 return Token("BAD", lexeme, self.line, start_col + 1)
+        #             if "." not in lexeme and int(lexeme) > 32_767:
+        #                 return Token("BAD", lexeme, self.line, start_col + 1)
+        #             token_type = "FLOAT" if "." in lexeme else "INTEGER"
+        #             return Token(token_type, lexeme, self.line, start_col + 1)
+
+        #         # Операторы и знаки пунктуации
+        #         if char in self.operators:
+        #             start_col = self.column
+        #             lexeme = self.read_operator()
+        #             token_type = self.get_operator_type(lexeme)
+        #             return Token(token_type, lexeme, self.line, start_col + 1)
+
+        #         # Некорректные токены
+        #         start_col = self.column
+        #         self.column += 1
+        #         return Token("BAD", char, self.line, start_col + 1)
+
+        #     self.current_line = ""  # Переход к следующей строке
 
     def read_while(self, condition):
         start = self.column
